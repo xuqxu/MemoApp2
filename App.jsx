c089779e0699311010060dcce032406f1a8218d0
xuqxu <froze666@me.com>
2021-07-18 00:52:31 +0900

    メモ入力画面作成

--- App.jsx
+++ App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
