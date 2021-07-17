fcab789fe0f002611eb8575d1b9f153b76ee00e5
xuqxu <froze666@me.com>
2021-07-17 22:56:11 +0900

    メモ一覧画面抽出

--- App.jsx
+++ App.jsx
@@ -1,26 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
+    <MemoListScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f6faff',
-  },
-});
---
+++ src/screens/MemoListScreen.jsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import AppBar from '../components/AppBar';
+import MemoList from '../components/MemoList';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoListScreen() {
+  return (
+    <View style={styles.container}>
+      {/* eslint-disable-next-line */}
+      <StatusBar style="auto" />
+      <AppBar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f6faff',
+  },
+});
