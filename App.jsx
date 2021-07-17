a3031ab96d640fe74e6e2e128e8d25d1e7a121a1
xuqxu <froze666@me.com>
2021-07-18 00:46:35 +0900

    メモ編集画面作成

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoListScreen />
     // <MemoDetailScreen />
+    <MemoEditScreen />
   );
 }
